eb5f74ceacbdb99e091f99f5cd211592e305a00b
TeezeeWOLF <126915251+TeezeeWOLF@users.noreply.github.com>
2024-03-11 00:33:39 -0700

    Heres in Hope that i didnt mess anything up :)

--- flask_app/controllers/chores.py
+++ flask_app/controllers/chores.py
@@ -1,77 +1,47 @@
 from flask_app import app
 from flask import render_template, redirect, request, session
-from flask_app.models import user # import entire file, rather than class, to avoid circular imports
+from flask_app.models import chore, parent # import entire file, rather than class, to avoid circular imports
 # As you add model files add them the the import above
 # This file is the second stop in Flask's thought process, here it looks for a route that matches the request
 
-# Create Users Controller
+# Create Chores Controller
 
 
-@app.post('/user/register')
-def create_new_user():
-    if user.User.create_new_user(request.form):
-        return redirect('/#') # redirect to the show page
-    return redirect('/')
-
-
-
-
-# Read Users Controller
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.get('/#')
-def display_user_page():
-    print(session, "This is session") 
+@app.post('/create/chore')
+def create_chore_process():
     if 'user_id' not in session: return redirect('/')
-    user_data = user.User.get_user_by_id(session['user_id'])
-    # you will need a function to get info from the joining table here
-    return render_template('#.html', user = user_data, )
+    chore.Chore.create_chore(request.form)
+    return redirect('/chore/page')
+
+@app.get('/create/chore')
+def create_chore_page():
+    if 'user_id' not in session: return redirect('/')
+    return render_template('create_chore.html')
+
+
+# Read Chores Controller
     
-
-
-# Update Users Controller
-
-@app.post('/user/update')
-def update_user_account():
-    if 'user_id' not in session: return redirect('/')
-    user.User.update_user_info(request.form)
-    return redirect('/user/account')
-
-# Delete Users Controller
-
-@app.post('/user/account/delete')
-def delete_user_account():
-    if 'user_id' not in session: return redirect('/')
-    user.User.delete_user_account(session['user_id'])
-    session.clear()
-    return redirect('/')
-
-# login user
-
-@app.post('/user/login')
-def log_user_in():
-    if user.User.log_user_in(request.form):
-        return redirect('/#') # redirect to the show page
-    return redirect('/')
-
-
-# log user out
-
-@app.route('/user/logout')
-def log_user_out():
-    session.clear()
-    return redirect('/')
-
-
-# Update Users Controller
+@app.route('/chore/page')
+def chore_Home_page():
+    return render_template('chores_home.html')
 
 
 
-# Delete Users Controller
+
+
+
+# Update Chores Controller
+
+
+# Delete Chores Controller
+
+
+
+# Update Chores Controller
+
+
+
+# Delete Chores Controller
 
 
 
